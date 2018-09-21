292215ef7c989d5d12340571db0a46618d6fabf3
youziliang <345717321@qq.com>
2018-09-22 00:55:24 +0800

    ...

--- src/main/java/com/whatu1/es/controller/RestHighController.java
+++ src/main/java/com/whatu1/es/controller/RestHighController.java
@@ -1,5 +1,9 @@
 package com.whatu1.es.controller;
 
+import org.elasticsearch.client.RestHighLevelClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("high")
 public class RestHighController {
 
+	private static final Logger logger = LoggerFactory.getLogger(RestLowController.class);
+
+	@Autowired
+	private RestHighLevelClient client;
 
 }
